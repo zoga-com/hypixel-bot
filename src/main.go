42de58d24dc48f823c17a4cc4c187fa7f78761e2
zemlyak <zemlyak@pandemonica.ru>
2021-08-21 18:35:55 +0300

    лонгполл в отдельный файл

--- src/main.go
+++ src/main.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"context"
-	"hypixel-bot/src/commands"
-	"hypixel-bot/src/util"
-	"log"
-
-	"github.com/SevereCloud/vksdk/v2/api"
-	"github.com/SevereCloud/vksdk/v2/events"
-	"github.com/SevereCloud/vksdk/v2/longpoll-bot"
+	"hypixel-bot/src/longpoll"
 )
 
 func main() {
-	vk := api.NewVK(util.Token)
-
-	group, err := vk.GroupsGetByID(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lp, err := longpoll.NewLongPoll(vk, group[0].ID)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lp.MessageNew(func(_ context.Context, obj events.MessageNewObject) {
-		log.Printf("%d: %s from %d", obj.Message.PeerID, obj.Message.Text, obj.Message.FromID)
-		commands.FindCommand(obj)
-	})
-
-	log.Println("Start Long Poll")
-	if err := lp.Run(); err != nil {
-		log.Fatal(err)
-	}
+	longpoll.StartLongpoll()
 }
