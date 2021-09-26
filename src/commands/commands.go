eba01653889d02abab7d11723e7f8174004b7bce
zemlyak <zemlyak@pandemonica.ru>
2021-09-26 14:59:58 +0300

    чето в командах поменял

--- src/commands/commands.go
+++ src/commands/commands.go
@@ -2,9 +2,10 @@ package commands
 
 import (
 	"errors"
-	"fmt"
 	"hypixel-bot/src/util"
+	"fmt"
 	"regexp"
+	"strings"
 	"strconv"
 	"sync"
 
@@ -16,11 +17,11 @@ var Commands = []*util.Command{Bedwars, Skywars, Skyblock, Nick}
 
 func FindCommand(obj events.MessageNewObject) {
 	groups := commandRegex.FindStringSubmatch(obj.Message.Text)
-	var args []string
+	var args []string = strings.Split(groups[2], " ")
 
 	if groups != nil {
-		if len(regexp.MustCompile(" ").Split(groups[2], -1)) <= 2 {
-			args = regexp.MustCompile(" ").Split(groups[2], -1) // самый отвратительный код в проекте
+		if len(args) <= 2 {
+			args = args[1:]
 		} else {
 			args = []string{}
 		}
@@ -30,7 +31,7 @@ func FindCommand(obj events.MessageNewObject) {
 			Args int
 		}{
 			Name: groups[1],
-			Args: len(args) - 1,
+			Args: len(args),
 		}
 
 		var wg sync.WaitGroup
@@ -43,12 +44,12 @@ func FindCommand(obj events.MessageNewObject) {
 				}
 
 				if it.Args == command.Args {
-					if util.MatchUsername(args[1]) {
-						err = it.Trigger(args[1], obj.Message.PeerID, obj.Message.FromID)
+					if util.MatchUsername(args[0]) {
+						err = it.Trigger(args[0], obj.Message.PeerID, obj.Message.FromID)
 					} else {
 						err = errors.New("Несуществующий ник.")
 					}
-				} else {
+				} else if it.Args != 0 {
 					row := util.DB.QueryRow("SELECT name FROM users WHERE id=" + strconv.FormatInt(int64(obj.Message.FromID), 10))
 					var name string
 					err = row.Scan(&name)
